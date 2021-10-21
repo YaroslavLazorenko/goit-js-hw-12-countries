7544af7b432f09fb4532d4dd68dfa76a607dcbbe
YaroslavLazorenko <lazorenko85@gmail.com>
2021-10-22 02:39:40 +0300

    Update fetchCountries with async/await

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
-export default function fetchCountries(searchQuery) {
-  const baseUrl = 'https://restcountries.com/v2/name/';
-  const url = baseUrl + searchQuery;
+export default async function fetchCountries(searchQuery) {
+  const BASE_URL = 'https://restcountries.com/v2/name/';
+  const url = BASE_URL + searchQuery;
 
-  return axios.get(url).then(response => {
-    if (response.status === 200) return response.data;
-    throw new Error('Error fetching data');
-  });
+  const response = await axios.get(url);
+  if (response.status === 200) return response.data;
+  throw new Error('Error fetching data');
 }
